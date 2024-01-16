74fd3cbe8e44d853c34a73c545a44afcd421dc37
justinbrown15 <justin.brown15@shhu.edu>
2024-01-16 15:23:21 -0500

    added bullets to the game and allowed for only 3 to be on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 #class which manages game assets and behavior
 class AlienInvasion:
     def __init__(self):
@@ -15,12 +16,18 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         #sets the bg color
-        self.bg_color = (55, 82, 155)
+        #self.bg_color = (55, 82, 155)
+        self.bg_color = (129, 0, 255)
+
+        
 
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             #make the most recently drawn screen visible
             pygame.display.flip()
 
@@ -29,6 +36,7 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                   sys.exit()
+                
                 elif event.type == pygame.KEYDOWN:
                      self._check_keydown_events(event)
                 
@@ -40,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_right = True
          elif event.key == pygame.K_a:
             self.ship.moving_left = True
+         elif event.key == pygame.K_SPACE:
+             self._fire_bullet()
          elif event.key == pygame.K_ESCAPE:
              sys.exit()
     def _check_keyup_events(self, event):
@@ -47,13 +57,28 @@ class AlienInvasion:
             self.ship.moving_right = False
                 
          elif event.key == pygame.K_a:
-            self.ship.moving_left = False              
+            self.ship.moving_left = False
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                print(len(self.bullets))            
     def run_game(self):
         #starts the main loop for the game
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
+            #gets rid of bullets that have disappeared off the screen
+            
+            
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -6,7 +6,13 @@ class Settings:
        
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (255, 39, 225)
-
+        #self.bg_color = (255, 39, 225)
+        self.bg_color = (129, 0, 255)
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
         #ship speed
         self.ship_speed = 1
